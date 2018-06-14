72e8898852bd5aba042a4c1810f5a8df4e182390
octaguzz <octaguzz@outlook.com>
2018-06-14 21:45:42 +0100

    Commit inicial com todo o projecto feito. Dia 14/06/2018

---
+++ MVCKnockout/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKnockout.Models;
+
+namespace MVCKnockout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Basic()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Advanced() {
+
+            var person = new Person
+            {
+                FirstName = "Gustavo",
+                LastName = "Reis"
+            };
+
+            return View(person);
+        }
+    }
+}
